cb80392505e1ffff272ab1afecb7f651c032595a
Carl Yeksigian <cyeksigian@bluemountaincapital.com>
2013-06-28 06:54:40 -0400

    Update to version 26

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 # Install Brackets
-class brackets ($version='25') {
+class brackets ($version='26') {
   package { "brackets-sprint-${version}-OSX":
     provider => 'appdmg',
     source   => "http://download.brackets.io/file.cfm?platform=OSX&build=${version}&_=.dmg",
